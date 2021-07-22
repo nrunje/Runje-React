069f7eb51cf2821eafb14240ca286a51a6da2526
Nick Runje <nickrunje@gmail.com>
2021-07-21 18:16:18 -0700

    Adds NavBar and app container

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+        <div className="nav-container">
+            <nav>
+                <p>Ham</p>
+                <h1>Nicholas Runje</h1>
+                <p>Dark</p>
+            </nav>
+
+        </div>
+    );
+}
