6680eb724f25002f088bfe4e6dac6dcf10561151
Nick Runje <nickrunje@gmail.com>
2021-07-28 17:28:37 -0700

    Adds homepage marker to make produciton build viewable on local computer

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./NavBar.css";
 import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
 import BrightnessMediumIcon from '@material-ui/icons/BrightnessMedium';
 
 export default function NavBar() {
     return (
         <div className="nav-container">
             <nav>
+                <IconButton>
                 <MenuIcon />
-                <h1>Nicholas Runje</h1>
+                </IconButton>
+                <h1>Runje.org</h1>
+                <IconButton>
                 <BrightnessMediumIcon />
+                </IconButton>
             </nav>
 
         </div>
