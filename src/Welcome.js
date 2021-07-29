b2e5c9e98fc0e0d76d03100be41b99e9def11fcc
Nick Runje <nickrunje@gmail.com>
2021-07-28 17:10:49 -0700

    Puts welcome sign in middle of div

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ import Typing from "react-typing-animation";
 export default function Welcome() {
     return (
         <div className="welcome-sign">
-            <Typing speed={100}>
+            <Typing speed={100} className="welcome-typing">
                 <Typing.Delay ms={500} />
                 <span>Welcome to my website.</span>
                 <Typing.Rest count={1} delay={500} />
