411f18747ceda4145b7e4e6666d472f454f1dc33
Nick Runje <nickrunje@gmail.com>
2021-07-30 19:49:58 -0700

    React Router works on live site but can't directly load

--- src/About.js
+++ src/About.js
@@ -1,10 +1,16 @@
 import { AddBoxOutlined } from "@material-ui/icons";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function About(props) {
-    return (<div>
-        On about page
-    </div>);
+    return (
+        <div>
+            <p>ON about page</p>
+            <Link to="/">
+                <h1>Go back to home page</h1>
+            </Link>
+        </div>
+    );
 }
 
 export default About;
