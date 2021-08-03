99dfc3f5f84ccaeebf933f56d938e3d752ed7612
Nick Runje <nickrunje@gmail.com>
2021-08-03 10:22:58 -0700

    Adds hover darkener

--- src/Writing.js
+++ src/Writing.js
@@ -10,13 +10,18 @@ export default function Writing() {
             <NavBar />
 
             <RoundedBox pagetitle="Writings &#38; Other Essays">
-                <ProjectCard image={CompImage} />
-                {/* <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard /> */}
+                <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                </ProjectCard>
+                <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                </ProjectCard>
+                <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                </ProjectCard>
+                <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+                </ProjectCard>
             </RoundedBox>
         </div>
     );
