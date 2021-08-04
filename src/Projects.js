5f7a0500eec05e3c55940916d6366e82e0176ba8
Nick Runje <nickrunje@gmail.com>
2021-08-04 11:19:33 -0700

    Adds package.json file and begins to make the addition of new cards automatic

--- src/Projects.js
+++ src/Projects.js
@@ -4,8 +4,17 @@ import NavBar from "./NavBar";
 import RoundedBox from "./RoundedBox";
 import { ProjectCard } from "./ProjectCard";
 import CompImage from "./computer.jpg";
+import ProjectsData from "./projects.json";
 
 export default class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            projects: ProjectsData
+        }
+    }
+
     render() {
         return (
             <div className="projects-page">
@@ -13,7 +22,7 @@ export default class Projects extends React.Component {
 
                 <RoundedBox pagetitle="Personal CS Projects">
                     <a href="https://sorting.runje.org">
-                    <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
+                    <ProjectCard image={require("./computer.jpg")} title="Test Item">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore 
                      magna aliqua. Quis ipsum suspendisse ultrices gravida. 
@@ -21,14 +30,13 @@ export default class Projects extends React.Component {
                     </ProjectCard>
                     </a>
 
-                    <a href="https://sorting.runje.org">
-                    <ProjectCard image={CompImage} title="Sorting Algos Visualizer">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                     sed do eiusmod tempor incididunt ut labore et dolore 
-                     magna aliqua. Quis ipsum suspendisse ultrices gravida. 
-                     Risus commodo viverra maecenas accumsan lacus vel facilisis. 
-                    </ProjectCard>
-                    </a>
+                    {this.state.projects.map(project => (
+                        <a href={project.link}>
+                            <ProjectCard key={project.id} title={project.title} image={project.image}>
+                                {project.description}
+                            </ProjectCard>
+                        </a>
+                    ))}
                 </RoundedBox>
             </div>
         );
