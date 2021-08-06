cfb733d2b71b709206a3e1efdaedbbd3ab72c82e
Nick Runje <nickrunje@gmail.com>
2021-08-06 12:13:23 -0700

    Adds temporary fix to project images

--- src/Projects.js
+++ src/Projects.js
@@ -3,8 +3,13 @@ import "./Projects.css";
 import NavBar from "./NavBar";
 import RoundedBox from "./RoundedBox";
 import { ProjectCard } from "./ProjectCard";
-import CompImage from "./computer.jpg";
 import ProjectsData from "./projects.json";
+
+// Temporary images until JSON relative image paths can be fixed
+import CompImage from "./computer.jpg";
+import FountainPen from "./fountain-pen.jpg";
+import BlueOcean from "./blue-ocean-compressed.jpg";
+// ====================================
 
 export default class Projects extends React.Component {
     constructor(props) {
@@ -21,24 +26,21 @@ export default class Projects extends React.Component {
                 <NavBar />
 
                 <RoundedBox pagetitle="Personal CS Projects">
-                    <a href="https://sorting.runje.org">
-                    <ProjectCard image={require("./computer.jpg")} title="Test Item">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                     sed do eiusmod tempor incididunt ut labore et dolore 
-                     magna aliqua. Quis ipsum suspendisse ultrices gravida. 
-                     Risus commodo viverra maecenas accumsan lacus vel facilisis. 
-                    </ProjectCard>
-                    </a>
-
+                    <ul>
                     {this.state.projects.map(project => (
                         <a href={project.link}>
-                            <ProjectCard key={project.id} title={project.title} image={project.image}>
+                            <ProjectCard key={project.id} title={project.title} image={images[parseInt(project.id)]}>
                                 {project.description}
                             </ProjectCard>
                         </a>
                     ))}
+                    </ul>
+
                 </RoundedBox>
             </div>
         );
     }
 }
+
+// Temporary way to resolve images due to unknown error linking images in JSON file
+const images = [CompImage, FountainPen, BlueOcean]
