a0ba29963a88bce2acb615eebe8bc152c38692a6
Nick Runje <nickrunje@gmail.com>
2021-08-06 19:38:16 -0700

    Sets up preliminary about content format

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@ import { AddBoxOutlined } from "@material-ui/icons";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./About.css";
+import Person from "./person.jpg";
 
 function About(props) {
     const [date] = useState(new Date());
@@ -14,8 +15,23 @@ function About(props) {
             </Link>
             </div>
 
-            <div className="about-content-container">
-                Test
+            <div className="about-content-total-container">
+                <div className="about-content-container">
+                    <div className="about-content-upper">
+                        <div className="about-content-upper-left">
+                            <img src={Person} />
+                        </div>
+
+                    <div className="about-content-upper-right">
+                        <h1>Upper right</h1>
+                    </div>
+                    </div>
+
+
+                    <div className="about-content-lower">
+                        Buttons here
+                    </div>
+                </div>
             </div>
 
             <footer>
