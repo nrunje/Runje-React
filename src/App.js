1bd8c8229c02c4dc45838e28393a716851141c35
Nick Runje <nickrunje@gmail.com>
2021-08-26 10:09:35 -0400

    Adds unfinished resume page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Error404 from "./pages/Error404";
+import Resume from "./pages/Resume";
 
 
   export default function App() {
@@ -13,6 +14,9 @@ import Error404 from "./pages/Error404";
         <Switch>
           <Route path="/projects" exact component={Projects} />
           <Route path="/about" exact component={About} />
+          <Route path="/resume">
+            <Resume />
+          </Route>
           <Route path="/" exact component={Home} />
           <Route path="/" component={Error404} />
         </Switch>
